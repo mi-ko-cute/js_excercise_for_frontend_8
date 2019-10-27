643f1e53edba9ae3fa44c50c4d1b6e3b48337ead
mi-ko-cute <thx-mail@worysolve.com>
2019-10-27 09:46:48 +0900

    マージした内容を反映

--- main.js
+++ main.js
@@ -147,8 +147,8 @@
   // 正解・不正解の解答をシャッフルする。
   function buildQuiz(quiz) {
     const answers = quiz.correct_answer.concat(incorrect_answer);
-    const shuffledQuiz = shuffle(answers);
-    return shuffledQuiz;
+    const shuffledAnswers = shuffle(answers);
+    return shuffledAnswers;
   }
 
 
