59cd1244d32702eba0463a511bcdfdec0efea540
mi-ko-cute <thx-mail@worysolve.com>
2019-10-27 09:13:44 +0900

    var div をconst div に修正

--- main.js
+++ main.js
@@ -170,7 +170,7 @@
   // - 戻り値
   //   - 文字列
   function unescapeHTML(str) {
-    var div = document.createElement("div");
+    const div = document.createElement("div");
     div.innerHTML = str.replace(/</g, "&lt;")
       .replace(/>/g, "&gt;")
       .replace(/ /g, "&nbsp;")
